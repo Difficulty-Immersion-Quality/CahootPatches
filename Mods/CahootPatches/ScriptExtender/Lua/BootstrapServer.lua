89efa2abc780b4ea94f9d4cf0c25a16afc2b3c87
Cahooot <kenneth.zhaoo@gmail.com>
2025-10-18 00:56:41 -0400

    Remove Skill Actions patch

--- Mods/CahootPatches/ScriptExtender/Lua/BootstrapServer.lua
+++ Mods/CahootPatches/ScriptExtender/Lua/BootstrapServer.lua
@@ -1,8 +1,8 @@
--- SkillActions: spell container patch stolenf rom Goon
+-- -- SkillActions: spell container patch stolenf rom Goon
 
-Ext.Osiris.RegisterListener("LevelGameplayStarted", 2, "after", function(level, isEditorMode)
-    local squadies = Osi.DB_PartOfTheTeam:Get(nil)
-    for _,k in pairs(squadies) do
-		AddSpell(k[1], "Nemid_Skill_Actions_Container", 1, 1)
-    end
-end)
+-- Ext.Osiris.RegisterListener("LevelGameplayStarted", 2, "after", function(level, isEditorMode)
+--     local squadies = Osi.DB_PartOfTheTeam:Get(nil)
+--     for _,k in pairs(squadies) do
+-- 		AddSpell(k[1], "Nemid_Skill_Actions_Container", 1, 1)
+--     end
+-- end)
